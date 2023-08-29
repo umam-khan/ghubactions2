c77473f1f0ac55ac0f581f0649530f14a1a55b9c
ArunPerkash <98771345+ArunPerkash@users.noreply.github.com>
2023-08-30 01:45:49 +0530

    add unit test

--- app/src/test/java/com/example/firstapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/firstapp/ExampleUnitTest.kt
@@ -1,17 +1,13 @@
 package com.example.firstapp
 
 import org.junit.Test
-
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+class GreetingTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testGreetingText() {
+        val name = "Arun"
+        val greetingText = Greeting(name)
+        assertEquals("Hello, my name is $name!", greetingText)
     }
 }
